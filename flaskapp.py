fbdf743e6a8a6a7967fd24a6726c5834ebc4d5d9
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2020-04-01 04:38:54 +0800

    fix download filename and link

--- flaskapp.py
+++ flaskapp.py
@@ -10,10 +10,13 @@ def	index():
 def	aj_pryor():
 	return render_template('Detailed_Cleaning_Visualization_Python.html')
 
-@app.route('/aj_pryor_download')
+@app.route('/aj_pryor_download_notebook')
 def	aj_pryor_download():
-	return send_file('resources/alan_jupyter.ipynb')
+	return send_file('resources/alan_jupyter.ipynb', as_attachment=True)
 
+@app.route('/openvpn')
+def vpn():
+    return send_file('resources/rsbpi.ovpn', as_attachment=True)
 
 if __name__ == '__main__':
 	app.run()
